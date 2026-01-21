8998879e11aa9b7be36e26c7cfd79f1bf7594c17
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-01-21 09:29:34 +0000

    container change done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "sanketbhosale11575@gmail.com"
+var accountPassword = "123456"
+accountCity = "Pune"
+let accountState ;
+/*
+note : not use a var issue in block scope and functional scope
+*/
+// accountId = 2 
+
+console.log(accountId);
+
+accountEmail ="bhosalesanket746@gmail.com"
+
+console.table([accountEmail,accountState,accountPassword,accountCity,accountId])
