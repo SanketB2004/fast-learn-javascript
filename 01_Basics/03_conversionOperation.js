73b9ee101b518f379e0ce35eb6c5a91232835274
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-01 03:13:30 +0000

    done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -14,7 +14,7 @@ console.log(typeof(valueInNumber));
 // null => 0
 //undefined => NaN
 
-let is = 1;
+let is = 1;// conversion from number to boolean
 let booleanIsLoggedIn = Boolean(is);
 console.log(booleanIsLoggedIn);
 
@@ -26,7 +26,7 @@ console.log(booleanIsLoggedIn);
 // undefined => false
 // NaN => false
 
-let Numberr = 33;
+let Numberr = 33;// conversion from number to string
 console.log(Numberr);
 
 let scoreInString = String(Numberr);
@@ -37,3 +37,48 @@ console.log(typeof(scoreInString));
 // false => "false"
 // null => "null"
 // undefined => "undefined"
+
+// ************************* operations *********************** 
+
+let value = 3 
+let negValue = -value // unary negation
+console.log(negValue); //note the output
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(5%2);
+console.log(2**3);
+                     // modulus operator
+
+     let str1 = "hello" 
+     let str2 = "world" 
+     
+     let str3 = str1 +   str2 // string concatenation
+        console.log(str3);
+
+        console.log("1"+2); // "12"
+        console.log(1+"2"); // "12"
+        console.log("1"+2+2); // "122" // concatenation happens first 
+        console.log(1+2+"2"); // "32" // addition happens first then concatenation
+        
+console.log(4-7 / 2 + 3 * 6); // operator precedence // not a useful expression just for demo
+
+console.log(+true); // unary plus operator // output 1
+console.log(+false); // unary plus operator // output 0
+
+// not use tricky conversions like these in real code 
+
+let num1 = num2 = num3 = 2+2; // chaining assignment operators
+console.log(num1, num2, num3);
+
+
+
+let point = 10;
+++point; // prefix increment // increments point by 1 // 
+console.log(point);
+point--; // postfix decrement // decrements point by 1
+console.log(point);
+
+// completed 
