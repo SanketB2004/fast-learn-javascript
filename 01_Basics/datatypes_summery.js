9f1f256ca5668b61e76e2c7733355406c98c70cd
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-01 04:27:00 +0000

    done

---
+++ 01_Basics/datatypes_summery.js
@@ -0,0 +1,35 @@
+//Primitive 
+
+// 7 types : string , bigint , symbole , number , undefiend , null , boolean
+
+const isloginuser = 4558852755n;// last add only n to desided to bigint use here datatype
+console.log(typeof(isloginuser));
+
+// javascript is dynamic typed language 
+
+// referance types or non primitive datatypes 
+
+// arrays ,objects ,functions 
+ // basics example
+const heroes = ["sanket", "atherv"]//array
+
+let obj = {
+   name:"sanket ",
+   age:22 
+} //object
+
+// simple basics funcation example 
+// function data types is function 
+const myFunc = function(){
+    console.log("Hello World");
+    
+}
+
+
+// data type serch use a typeof operator 
+
+console.log(typeof(isloginuser));
+
+
+
+
