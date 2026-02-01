9a405693d34223d6d336c9fdc3e6370daa2c31a5
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-01 04:04:19 +0000

    done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1); 
+// console.log(2 >= 2);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);// avoid comparision this code only clean code prority only clean code
+console.log("02" > 1);// avoid comparision this code only clean code prority only clean code
+
+console.log(null > 0);// avoid comparision this code only clean code prority only clean code
+console.log(null == 0);// avoid comparision this code only clean code prority only clean code
+console.log(null >= 0)// avoid comparision this code only clean code prority only clean code
+console.log(undefined == 0)
+// avoid comparision this code only clean code prority only clean code
+// Comparision and equlity check is differnt methods 
+
+// === strict check 
+
+console.log("2" === 2); 
+
+
+
+
+
+
+
