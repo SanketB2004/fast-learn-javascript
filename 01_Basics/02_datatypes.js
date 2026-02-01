2acb5690d0d0a373cb9b0abfc4af6c70b1eac836
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-01 02:25:28 +0000

    push of 02

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // is use new version of javascript  
+//alert("Hello World") // we are using node js not browser so alert not work 
+
+console.log("3+3")
+
+// datatype 
+
+let name = "Sanket Bhosale" // string datatype 
+let age = 23 // number datatype
+let isApproved = true // boolean datatype
+let firstName = undefined // undefined datatype
+let lastName = null // null datatype // typeof null is object
+let symbol = Symbol("id") // symbol datatype
+
+//output the datatype
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isApproved)
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof symbol)
+
+//typeof is use to know the datatype of variable
+
