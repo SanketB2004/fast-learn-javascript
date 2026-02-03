0a710d80d61a9e4c15687ffb9b50e65b88a470d0
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-03 07:59:26 +0000

    string

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,37 @@
+const name = "Sanket "
+const repoCount = 50
+
+// console.log(name + repoCount + " Hi");
+
+console.log(`Heloo myname is ${name} and my repo count is a ${repoCount}`);
+
+
+const gameName = new String('Sanket') // String Declered Methood 
+
+
+console.log(gameName[0]);//Key Valu Access
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(5));
+
+console.log(gameName.indexOf('t'));
+
+console.log(gameName.substring(0,3));
+
+console.log(gameName.slice(-5,2));
+
+const newStringOne = "  Sanket   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+//replace 
+console.log(gameName.replace('Sanket','Bhosale'));
+
+console.log(gameName.includes('Sanket'));
+
+//array covert string 
+
+console.log(gameName.split('-'));
