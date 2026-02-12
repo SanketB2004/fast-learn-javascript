4a1fc439f110a756e1c439e01419e0f6ec2d8c5f
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-12 05:36:49 +0000

    Date&Time Basics

---
+++ 01_Basics/09_date&time.js
@@ -0,0 +1,36 @@
+// console.log("Hii");
+
+//  let myDate =new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof(myDate ));
+
+//let myCreateddate = new Date(2023,0,23)
+//console.log(myCreateddate.toDateString());
+
+// time add 
+// let myCreateddate = new Date("01-14-2023")
+// console.log(myCreateddate.toLocaleString());
+
+// // 
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateddate.getTime());
+
+//console.log(Math.floor(Date.now()/1000));
+
+ let newDate = new Date()
+// console.log(newDate.getDay());
+
+// custamize formate 
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    
+});
+
+//${newDate.getDay()} -- asa pn karu shakato
