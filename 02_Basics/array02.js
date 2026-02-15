40c2dcfa941a371197842a770f9b39c5f3f152f5
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-15 12:15:12 +0000

    Array.of Complete

--- 02_Basics/array02.js
+++ 02_Basics/array02.js
@@ -32,3 +32,6 @@ console.log(Array.from("sanket"));
 
 
 let score1 = 100 ;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1 , score2 , score3)); // THIS METHOD TO USE MULTIPLE VARIABLES IN INCLUDES ONE VARIABLE TO USE ARRAY METHOD OF FUNCATION
