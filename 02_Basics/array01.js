db7639d23839b081e14fe18c297ce82355ada755
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-15 11:31:17 +0000

    array complete

---
+++ 02_Basics/array01.js
@@ -0,0 +1,53 @@
+// array 
+
+const myArr = [0,1,2,3,4,5]
+
+// const myHeros = ["shakatiman", "sanket"]
+
+// const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr[0]);
+
+
+// array methods 
+
+// myArr.push(6)
+// myArr.push(7) //push method
+
+// myArr.pop() // last value cut  
+// console.log(myArr);  
+
+// myArr.unshift(9) // add first letter 
+
+// myArr.shift() // one line output find
+
+// console.log(myArr.includes(9)); // serach true and false
+
+// console.log(myArr.indexOf(3)); // index cheking 
+
+
+// const ne = myArr.join(); // join but type change string type make 
+
+
+
+// // console.log(typeof ne);
+
+
+// // slice , splice 
+
+
+// console.log(("A ", myArr));
+
+// const myn1 = myArr.slice(1 , 3) // slice not change a main array only xerox of array 
+
+
+// console.log(myn1);
+
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2);
+
+
+// console.log("C ",myArr ); // origanal array change in splice chack a poration  1 , 2 , 3 this value is cut in array this is a splice
+// // and renge defferent 
+
---
+++ 02_Basics/array02.js
@@ -0,0 +1,34 @@
+const marvel_heroes =["thor","ironman","spiderman"]
+
+const dc_heroes=["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes); 
+//console.log(marvel_heroes[3][1])
+
+// let allheroes =marvel_heroes.concat(dc_heroes)
+// console.log(allheroes);
+
+// spred operator 
+
+// const all_new_heroes=[...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heroes);
+
+
+// new method
+ 
+const another_array =[1,2,3,[4,5,6],7,[5,7,[5,8]]]
+
+const real_another_array = another_array.flat(Infinity)//nytr tu depth lauu shaktoo asa kiti array ahet techa no
+
+console.log(real_another_array);
+
+
+// from use karun array make karu shakato 
+
+console.log(Array.isArray("sanket"));
+console.log(Array.from("sanket"));
+
+
+let score1 = 100 ;
