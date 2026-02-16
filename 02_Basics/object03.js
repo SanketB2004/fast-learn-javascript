b3e0b55eebfc2ae5ce51afd296837f448e9a4cce
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-16 06:14:56 +0000

    done

---
+++ 02_Basics/object03.js
@@ -0,0 +1,41 @@
+//object destructuring and json api 
+
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor: "sanket"
+}
+
+// const {courseInstructor:instructor} = course // this name change it called destructuring
+
+// console.log(instructor);//
+
+
+/// api consepts 
+
+// {
+// "name":"sanket",
+// "coursename":"js in hindi",
+// "peice":"free"
+// } // json demo api
+
+
+// [   {},
+//     {},
+//     {}
+// ]// demo api
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
