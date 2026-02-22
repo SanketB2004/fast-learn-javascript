4ebbd177765d5388a9715354a0743d4621cdb2ee
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-22 07:48:29 +0000

    Done

---
+++ 04_control_Flow/truthi&fal.js
@@ -0,0 +1,16 @@
+// const userEmail = ""
+
+// if (userEmail) {
+//     console.log("Got user email");
+    
+// }else {
+//     console.log("Dont have user email");
+    
+// } 
+
+// falsy value 
+// false , 0 , -0, Bigint 0n , "" , null , undefiend , NaN 
+
+// truty value 
+// { } , [ ] , "0" , funstion() {} 
+
