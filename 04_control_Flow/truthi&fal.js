7ac913d1e4c6772bcba106c65b7678e00673f0b2
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-22 08:07:36 +0000

    okkkkkkkk

--- 04_control_Flow/truthi&fal.js
+++ 04_control_Flow/truthi&fal.js
@@ -14,3 +14,34 @@
 // truty value 
 // { } , [ ] , "0" , funstion() {} 
 
+// const empmpty = {} // check object empty ahe ka ny 
+
+// if (Object.keys(emptyObj).length === 0) {
+
+//     console.log("Object is empty");
+    
+    
+// }
+ 
+
+// nullish coalesing operator (??):null undefiend 
+
+let val1 ;
+
+// // val1 = 5 ?? 10 
+
+// // val1 = null ?? 10
+
+// // val1 = undefined ?? 15
+
+// val1 = null ?? 105 ?? 155
+
+
+// console.log(val1);
+
+// ternary operator  ---------------------------------------------------------
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log("Less Than 80"): console.log("More Thsn 80");
+
