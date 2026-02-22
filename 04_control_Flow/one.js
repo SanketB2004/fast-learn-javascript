672b21092b8b1d971f9e3740e1a5ea6c267c8b6d
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-22 07:28:23 +0000

    one

---
+++ 04_control_Flow/one.js
@@ -0,0 +1,31 @@
+// // control flow in javascript 
+
+// //if
+
+// const isUserLooggedin = true ;
+
+
+// if(isUserLooggedin){
+    
+// }
+
+// // 
+// // dont this code use in industry
+// if(100>200) console.log("Done"),console.log(true);
+
+//  const balance = 1000;
+
+//  if (balance>1000) {
+//     console.log("Less than");
+    
+//  }else if (balance<750) {
+//     console.log("Less than 750");
+    
+//  }else{
+//  console.log("ooooo no balance minimum charges applicable 100 per month");
+ 
+ 
+//  }
+
+// comparision op 
+// logical op 
