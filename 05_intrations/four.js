f0beac481c1428b78ac05a0b95bf28fd2dcbb989
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-23 17:08:06 +0000

    Part 1 Done

---
+++ 05_intrations/four.js
@@ -0,0 +1,56 @@
+// filter map and reduce 
+
+//1// const coding = ["js", "ruby" , "java", "python" , "cpp"] 
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     // return item
+// })
+
+// console.log(values); // forEach loop not return any values 
+// asa return kartyt mng 
+
+////
+//
+//
+
+//2// const myNums =[1,2,3,4,5,6,7,8,9,10]
+
+// const newnums = myNums.filter(  (num) => num > 4 )
+
+// console.log(newnums);
+// this use in return in arrays perform the operation  basic code 
+// not use this code in scope of function {} not use this not use scope lavala scope ter use ker return
+// exmple same use scope {}
+
+ const myNums =[1,2,3,4,5,6,7,8,9,10]
+
+// const newnums = myNums.filter(  (num) =>  {
+//     return num > 4
+// })
+
+// console.log(newnums);
+
+
+//3// ata for each madhe same karu 
+
+// const newnum = []
+
+// myNums.forEach( (num) => {
+
+//     if (num > 4) {
+//         newnum.push(num)
+//     }
+// })
+// console.log(newnum);
+
+
+//4// ha yak q ahe cha ans serching cha ok book ter mala  (filtercha example ahe)
+//const userbook = books.filter( (bk) => bk.genre === 'History')
+
+
+
+
+
+
+
