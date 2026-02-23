d9fa9cae1afadb4c9d7cddf2380aa18681265ddb
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-23 06:50:58 +0000

    Done

--- 05_intrations/three.js
+++ 05_intrations/three.js
@@ -1,1 +1,54 @@
 // high order array loops 
+// for of 
+// const i = [1,2,3,4,5]
+// for (const num of i ) {
+//     console.log(num);
+    
+// }
+
+
+
+// const greet = "Hello world"
+
+// for (const greee of greet) {
+//     console.log(greee);
+    
+// }
+
+// Maps 
+
+// const map = new Map();
+// map.set('In' , "India")
+// map.set('Usa' , "Ameraca")
+// map.set('Russ' , "Russia")
+// map.set('Pok' , "Pakistan")
+// map.set('Thai' , "Thailand")
+
+// for (const [key , value] of map) {
+//     console.log(key , ':-' , value);
+    
+// }
+
+
+
+// my Object
+
+const myObject = {
+    'game1':'NFS',
+    'game2': 'Pubg',
+    'game3':'Free Fire'
+}
+
+// for (const [key , value] of myObject) {
+//     console.log(key , ':-' ,value);
+    
+// }
+
+const myObj = {
+js : 'javascript',
+cpp:'c++',
+rb:'ruby',
+swift: 'swift by apple'
+}
+
+
