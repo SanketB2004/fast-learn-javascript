0c067dc94ee6ca02549905a41411a94eb886f314
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-02-23 06:16:35 +0000

    Done

---
+++ 05_intrations/two.js
@@ -0,0 +1,17 @@
+// while and do-while loop 
+
+//  let i = 0;
+
+//  while (i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i++;
+//  }
+ 
+
+ // do - while loop 
+
+ let arr = 1;
+   do {
+    console.log(`Score is ${arr}`);
+    arr++;
+   } while (arr <= 10);
